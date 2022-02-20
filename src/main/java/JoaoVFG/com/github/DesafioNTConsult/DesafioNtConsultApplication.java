f5c34a4bc9a38b6ff4ca1a30e605cf7fdb70e42e
Joao Vitor Ferreira Garcia <joao.vfg@hotmail.com.br>
2022-02-20 11:29:39 -0300

    Inicializando Projeto Desafio NTConsult

---
+++ src/main/java/JoaoVFG/com/github/DesafioNTConsult/DesafioNtConsultApplication.java
@@ -0,0 +1,13 @@
+package JoaoVFG.com.github.DesafioNTConsult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioNtConsultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioNtConsultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JoaoVFG/com/github/DesafioNTConsult/DesafioNtConsultApplicationTests.java
@@ -0,0 +1,13 @@
+package JoaoVFG.com.github.DesafioNTConsult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioNtConsultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
