c8f908fe51c8d85ceda5e7e70510a372d1da2ddf
Joao Vitor Ferreira Garcia <joao.vfg@hotmail.com.br>
2022-02-21 00:31:57 -0300

    Adicionado Anotação de Geração de IDs nas classes de entidades

--- src/main/java/JoaoVFG/com/github/DesafioNTConsult/Entity/Pauta.java
+++ src/main/java/JoaoVFG/com/github/DesafioNTConsult/Entity/Pauta.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Getter
@@ -15,6 +17,7 @@ import javax.persistence.Id;
 public class Pauta {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String tema;
--- src/main/java/JoaoVFG/com/github/DesafioNTConsult/Entity/Voto.java
+++ src/main/java/JoaoVFG/com/github/DesafioNTConsult/Entity/Voto.java
@@ -2,10 +2,7 @@ package JoaoVFG.com.github.DesafioNTConsult.Entity;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Getter
 @Setter
@@ -16,6 +13,7 @@ import javax.persistence.ManyToOne;
 public class Voto {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @ManyToOne
