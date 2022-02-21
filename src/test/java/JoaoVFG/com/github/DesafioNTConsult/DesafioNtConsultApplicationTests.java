0c1cdf38fdef9facbcf5b2fce3716104701759d1
joao.garcia <joao.vfg@hotmail.com.br>
2022-02-21 12:34:23 -0300

    Testes para as Classes de Utilidade de Conversão de Data e Soma de Datas

--- src/test/java/JoaoVFG/com/github/DesafioNTConsult/DesafioNtConsultApplicationTests.java
+++ src/test/java/JoaoVFG/com/github/DesafioNTConsult/DesafioNtConsultApplicationTests.java
@@ -1,7 +1,12 @@
 package JoaoVFG.com.github.DesafioNTConsult;
 
+import JoaoVFG.com.github.DesafioNTConsult.Entity.Pauta;
 import JoaoVFG.com.github.DesafioNTConsult.Entity.Pessoa;
+import JoaoVFG.com.github.DesafioNTConsult.Repository.PautaRepository;
 import JoaoVFG.com.github.DesafioNTConsult.Repository.PessoaRepository;
+import JoaoVFG.com.github.DesafioNTConsult.Service.Util.DateParserUtil;
+import JoaoVFG.com.github.DesafioNTConsult.Service.Util.DateSum;
+import lombok.SneakyThrows;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +14,8 @@ import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,6 +26,10 @@ public class DesafioNtConsultApplicationTests {
 
     @Autowired
     PessoaRepository pessoaRepository;
+
+
+    DateParserUtil dateParserUtil = new DateParserUtil();
+    DateSum dateSum = new DateSum();
 
     @Test
     public void teste01_insertPessoa() {
@@ -40,6 +51,24 @@ public class DesafioNtConsultApplicationTests {
 
     }
 
+    @SneakyThrows
+    @Test
+    public void teste03_dateConversion() {
+        String strDate = "21/02/2022 08:00:00";
+        Date date = dateParserUtil.conversorData(strDate);
 
+        assertEquals("Mon Feb 21 08:00:00 BRT 2022",date.toString());
+
+    }
+
+    @SneakyThrows
+    @Test
+    public void teste04_dateSum() {
+        String strDate = "21/02/2022 08:00:00";
+        Date date = dateParserUtil.conversorData(strDate);
+        Date newDate = dateSum.sumMinutes(date, 60);
+        assertEquals("Mon Feb 21 09:00:00 BRT 2022",newDate.toString());
+
+    }
 
 }
