f6f174f479004f3393619417824899483c14c8c6
joao.garcia <joao.vfg@hotmail.com.br>
2022-02-21 16:52:05 -0300

    Inclusão de metodo de pesquisa na classe pessoa repository

--- src/main/java/JoaoVFG/com/github/DesafioNTConsult/Repository/PessoaRepository.java
+++ src/main/java/JoaoVFG/com/github/DesafioNTConsult/Repository/PessoaRepository.java
@@ -2,9 +2,19 @@ package JoaoVFG.com.github.DesafioNTConsult.Repository;
 
 import JoaoVFG.com.github.DesafioNTConsult.Entity.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
+
+    @Transactional(readOnly = true)
+    @Query("SELECT pessoa FROM Pessoa pessoa WHERE pessoa.id = :id")
+    public Pessoa findByPessoaId(@Param("id") Integer id);
+
 
 }
