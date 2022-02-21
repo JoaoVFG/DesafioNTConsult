ce2e023ba195c604c2618ddf1f60a115f647dd04
joao.garcia <joao.vfg@hotmail.com.br>
2022-02-21 16:51:21 -0300

    Criação da Packege para armazenar Classes de Exception

---
+++ src/main/java/JoaoVFG/com/github/DesafioNTConsult/Service/Exception/ObjectNotFoundException.java
@@ -0,0 +1,13 @@
+package JoaoVFG.com.github.DesafioNTConsult.Service.Exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException (String mensagem) {
+        super(mensagem);
+    }
+
+    public ObjectNotFoundException (String mensagem, Throwable cause) {
+        super(mensagem,cause);
+    }
+}
