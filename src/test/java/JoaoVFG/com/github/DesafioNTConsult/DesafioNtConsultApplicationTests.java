1d0f32b6cc1bd8e297fcb6a80066173bd126f519
Joao Vitor Ferreira Garcia <joao.vfg@hotmail.com.br>
2022-02-21 00:33:05 -0300

    Adicionado dependencia do Junit e criado testes para a Repositorio de pessoa

--- src/test/java/JoaoVFG/com/github/DesafioNTConsult/DesafioNtConsultApplicationTests.java
+++ src/test/java/JoaoVFG/com/github/DesafioNTConsult/DesafioNtConsultApplicationTests.java
@@ -1,13 +1,45 @@
 package JoaoVFG.com.github.DesafioNTConsult;
 
-import org.junit.jupiter.api.Test;
+import JoaoVFG.com.github.DesafioNTConsult.Entity.Pessoa;
+import JoaoVFG.com.github.DesafioNTConsult.Repository.PessoaRepository;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class DesafioNtConsultApplicationTests {
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class DesafioNtConsultApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    PessoaRepository pessoaRepository;
+
+    @Test
+    public void teste01_insertPessoa() {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setCpf("45567860889");
+        pessoa.setNome("Pessoa 1");
+        pessoaRepository.save(pessoa);
+
+    }
+
+    @Test
+    public void teste02_insertAndSearchPessoa() {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setCpf("11593054807");
+        pessoa.setNome("Pessoa A");
+        pessoaRepository.save(pessoa);
+
+        assertEquals(pessoaRepository.findById(2).get().getCpf(),"11593054807");
+
+    }
+
+
 
 }
