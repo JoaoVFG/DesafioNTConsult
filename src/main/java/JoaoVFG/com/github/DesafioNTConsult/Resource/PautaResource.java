5978cad4b01750a844deb98c15bb6c3cfb778b54
joao.garcia <joao.vfg@hotmail.com.br>
2022-02-22 16:29:02 -0300

    Desenvolvido Classe Pauta resource

---
+++ src/main/java/JoaoVFG/com/github/DesafioNTConsult/Resource/PautaResource.java
@@ -0,0 +1,37 @@
+package JoaoVFG.com.github.DesafioNTConsult.Resource;
+
+import JoaoVFG.com.github.DesafioNTConsult.DTO.StartVotacaoDTO;
+import JoaoVFG.com.github.DesafioNTConsult.Entity.Pauta;
+import JoaoVFG.com.github.DesafioNTConsult.Service.PautaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/pauta")
+public class PautaResource {
+
+    @Autowired
+    PautaService pautaService;
+
+    @RequestMapping(value = "/buscaid", method = RequestMethod.GET)
+    public ResponseEntity<Pauta> findById(@PathVariable String idPauta) {
+        Pauta pauta = pautaService.findById(Integer.parseInt(idPauta));
+        return ResponseEntity.ok().body(pauta);
+    }
+
+    @RequestMapping(value = "/inserepauta", method = RequestMethod.POST)
+    public ResponseEntity<Pauta> createPauta(@RequestBody String temaPauta) {
+        Pauta pauta = pautaService.createPauta(temaPauta);
+        URI uri = URI.create("/pauta" + "/buscaid" + pauta.getId());
+        return ResponseEntity.created(uri).body(pauta);
+    }
+
+    @RequestMapping(value = "/iniciavotacao", method = RequestMethod.POST)
+    public ResponseEntity<Pauta> startVotacao(@RequestBody StartVotacaoDTO startVotacaoDTO) {
+        Pauta pauta = pautaService.startVotacao(startVotacaoDTO);
+        return ResponseEntity.ok().body(pauta);
+    }
+}
